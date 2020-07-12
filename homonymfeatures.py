9f8a19f6d4a50ae7053abed3f94bf799f671231a
Natalia Tyulina <nataliatyulina@Natalias-MacBook-Pro.local>
2020-07-12 14:37:52 -0400

    Updated with the most recent version. DS_Store added to gitignore.

--- homonymfeatures.py
+++ homonymfeatures.py
@@ -4,13 +4,17 @@ import string
 import numpy as np
 import pymorphy2
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 from collections.abc import Iterable
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from spacy.lang.ru import Russian
 import re
 import maru
+from wiki_ru_wordnet import WikiWordnet
+from nltk.corpus import wordnet as wn
+from functools import reduce
+from operator import add
 
 class HomonymFeaturesException(Exception):
     def _init_ (self, *args):
@@ -33,21 +37,16 @@ class HomonymFeatures():
         if not ((len(target_pos_labels) == len(sentences) ) and (len(target_word_start) == len(sentences)) and (len(target_word_stop) == len(sentences))):
             raise HomonymFeaturesException("Unmatched input lengths")
 
-
         self.fulldata = pd.DataFrame(columns = ["sentence", "pos_label", "target_word_start", "target_word_stop", 'target_pos_label'])
-
         self.fulldata["sentence"] = sentences
         self.fulldata["target_pos_label"] = target_pos_labels
         self.fulldata["target_word_start"] = target_word_start
         self.fulldata["target_word_stop"] = target_word_stop
-
         self.allpos = ['adj', 'adp', 'adv', 'conj', 'det', 'h', 'intj', 'noun', 'part', 'pron', 'punct', 'unkn', 'verb']
-
         self.fulldata['target_pos_label'] = self.fulldata['target_pos_label'].apply(lambda x: self.allpos.index(x))
-
         self.target_word = target_word
 
-    def CreateTokensCorpus(self, tokenizer = "nltk", verbose = False):
+    def CreateTokenCorpus(self, tokenizer = "russian_tokenizer", verbose = False):
         if verbose:
             print("self.fulldata")
             print(self.fulldata)
@@ -56,8 +55,8 @@ class HomonymFeatures():
             target_pos = []
             for index, row in self.fulldata.iterrows():
                 tokenizedsen = nltk.word_tokenize(re.sub(r'[^\s\w_]+', ' ', row["sentence"]) )
-                extra = ['«', '»', '…', '―', '...', '№', '❤', '``', '\'', '..']
-                tokenizedsen = [t.casefold() for t in tokenizedsen if not t in string.punctuation and not t in extra and not t.isdigit()] # add user expansion of droplist
+                russian_stopwords = stopwords.words("russian")
+                tokenizedsen = [t.casefold() for t in tokenizedsen if not t in string.punctuation and not t in russian_stopwords and not t.isdigit()] # add user expansion of droplist
 
                 if verbose:
                     print(f"processing sentence {index}")
@@ -75,7 +74,26 @@ class HomonymFeatures():
             self.fulldata_words = fulldata_words
             return fulldata_words
         else:
-            raise NotImplementedError
+            target_pos = []
+            for index, row in self.fulldata.iterrows():
+                tokenizedsen = nltk.word_tokenize(re.sub(r'[^\s\w_]+', ' ', row["sentence"]), language = 'russian')
+                russian_stopwords = stopwords.words("russian")
+                tokenizedsen = [t.casefold() for t in tokenizedsen if not t in string.punctuation and not t in russian_stopwords and not t.isdigit()] # add user expansion of droplist
+                if verbose:
+                    print(f"processing sentence {index}")
+                    print(tokenizedsen)
+                word_num = 0
+                ### TODO ### Handle case where target word is in sent multiple times
+                print(tokenizedsen)
+                target_word_num = tokenizedsen.index(self.target_word) # add an exception if it's not found
+                #target_pos.append(self.allpos.index(row['word_id']) )
+                for token in tokenizedsen:
+                    fulldata_words = fulldata_words.append({"sentence_num": index, "word_num": word_num, "token": token, "target_pos_label": row["target_pos_label"], "target_word_num": target_word_num}, ignore_index = True)
+                    word_num += 1
+            if verbose:
+                print(fulldata_words)
+            self.fulldata_words = fulldata_words
+            return fulldata_words
 
     def CreatePosFeature(self, look, test = False, language = "ru", verbose = False):
         if language == "ru":
@@ -88,13 +106,13 @@ class HomonymFeatures():
                 return " ".join( context_pos )
             pos_sentences = self.fulldata_words.groupby("sentence_num").apply(create_pos_features)
             if test:
-                return pos_sentences.apply(lambda x: pd.Series(data = self.pos_vectorizer.transform([x]).toarray() [0], index = vectorizer.get_feature_names() ) )
+                return pos_sentences.apply(lambda x: pd.Series(data = self.pos_vectorizer.transform([x]).toarray() [0], index = vectorizer.get_feature_names() ))
             else:
                 return pos_sentences.apply(lambda x: pd.Series(data = vectorizer.transform([x]).toarray() [0], index = vectorizer.get_feature_names() ) )
         elif language == "en":
             raise NotImplementedError
         else:
-            NotImplementedError
+            raise NotImplementedError
 
     def CreateNearestPosFeature(self, look, language = "ru", verbose = False):
         if language == "ru":
@@ -146,26 +164,29 @@ class HomonymFeatures():
 
         return self.fulldata_words
 
-    def CreateLemmaFeature(self, look, language = "ru", test = False, verbose = False):
+    def FitLemmaFeature(self, train_indices, language = "ru", verbose = False):
+        #fulldata_words_train = self.fulldata_words.iloc[train_indices]
+        fulldata_words_train = self.fulldata_words[self.fulldata_words['sentence_num'].isin(train_indices)]
         if language == "ru":
-            if not test:
-                vectorizer = CountVectorizer()
-                vectorizer.fit([" ".join(self.fulldata_words['normal_forms'].values)])
-                self.lemma_vectorizer = vectorizer
+            vectorizer = CountVectorizer()
+            vectorizer.fit([" ".join(fulldata_words_train['normal_forms'].values)])
+            self.lemma_vectorizer = vectorizer
+            return vectorizer.get_feature_names()
+
+        elif language == "en":
+            raise NotImplementedError
+        else:
+            NotImplementedError
+
+    def TransformLemmaFeature(self, look, language = "ru", verbose = False):
+
             def create_lemma_features(g):
                 context_lemmas = g[( (g["word_num"] - g["target_word_num"]).abs() <= look ) & ~(g["word_num"] == g["target_word_num"] )]['normal_forms'].values
                 return " ".join( context_lemmas )
 
             lemma_sentences = self.fulldata_words.groupby("sentence_num").apply(create_lemma_features)
 
-            if test:
-                return lemma_sentences.apply(lambda x: pd.Series(data = self.lemma_vectorizer.transform([x]).toarray() [0], index = vectorizer.get_feature_names() ) )
-            else:
-                return lemma_sentences.apply(lambda x: pd.Series(data = vectorizer.transform([x]).toarray() [0], index = vectorizer.get_feature_names() ) )
-        elif language == "en":
-            raise NotImplementedError
-        else:
-            NotImplementedError
+            return lemma_sentences.apply(lambda x: pd.Series(data = self.lemma_vectorizer.transform([x]).toarray() [0], index = self.lemma_vectorizer.get_feature_names() ) )
 
     def CreateRpeCorpus(self, language = "ru", verbose = False):
         self.fulldata_words['rpe'] = self.fulldata_words['POS1'] + (self.fulldata_words['target_word_num'] - self.fulldata_words['word_num']).astype(str)
@@ -237,3 +258,71 @@ class HomonymFeatures():
             NotImplementedError
 
             return self.fulldata_words
+
+    def CreateSynsetCorpus(self, language = "ru", verbose = False):
+        if language == "ru":
+            wikiwordnet = WikiWordnet()
+            self.fulldata_words['synset'] = self.fulldata_words['token'].apply(lambda w: wikiwordnet.get_synsets(w))
+
+
+        elif language == "en":
+            raise NotImplementedError
+        else:
+            NotImplementedError
+
+        return self.fulldata_words
+
+    def CreateHypernymCorpus(self, language = "ru", verbose = False):
+        if language == "ru":
+            wikiwordnet = WikiWordnet()
+            self.fulldata_words['hypernym'] = self.fulldata_words['synset'][0].apply(lambda w: wikiwordnet.get_hypernyms(w))
+
+        elif language == "en":
+            raise NotImplementedError
+        else:
+            NotImplementedError
+
+        return self.fulldata_words
+
+    def FitWordNetFeature(self, train_indices, num_clues, language = "ru", verbose = False):
+        morph = pymorphy2.MorphAnalyzer()
+        all_clue_words = {}
+        pos_classes = self.fulldata_words['target_pos_label'].unique()
+        fulldata_words_train = self.fulldata_words[self.fulldata_words['sentence_num'].isin(train_indices)]
+        analyzer = maru.get_analyzer(tagger='linear', lemmatizer='dummy')
+
+        def get_pos(q):
+            analyzed = analyzer.analyze([q])
+            return list(analyzed)[0].tag.pos.value.casefold()
+        for pos_class in pos_classes:
+            all_syn = []
+            class_words = fulldata_words_train[fulldata_words_train['target_pos_label'] == pos_class]
+            class_words = class_words[class_words['token'] != self.target_word]
+            class_words = class_words[class_words['POS1'].isin(['noun', 'verb', 'adj', 'adv'])]
+
+            for s in class_words['synset'].values:
+                for w in s:
+                    for r in w.get_words():
+                        word = r.definition().split('~')[0]
+                        pos = get_pos(word)
+                        if pos in ['noun', 'verb', 'adj']:
+                            all_syn.append(morph.parse(word)[0].normal_form)
+                            print(word, pos, morph.parse(word)[0].normal_form )
+            clue_words = pd.Series(all_syn).value_counts().index.values
+            all_clue_words[pos_class] = clue_words
+
+        for w, v in all_clue_words.items():
+            all_clue_words[w] = [y for y in all_clue_words[w] if y not in [x for k in all_clue_words.keys() for x in all_clue_words[k] if k != w]]
+        all_clue_words = {k: v[:30] for k, v in all_clue_words.items()}
+        self.all_clue_words = all_clue_words
+        print(all_clue_words)
+        return all_clue_words
+
+    def TransformWordNetFeature(self, pos_class, language = "ru", verbose = False):
+        pos_class = self.allpos.index(pos_class)
+        c = CountVectorizer()
+        c.fit(self.all_clue_words[pos_class])
+        def create_clue_counts(g):
+            return pd.Series(data = c.transform([' '.join (g['normal_forms'].values)]).toarray()[0], index = c.get_feature_names())
+
+        return self.fulldata_words.groupby("sentence_num").apply(create_clue_counts)
--- test.py
+++ test.py
@@ -5,65 +5,87 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import confusion_matrix
 import fileinput
+from sklearn.model_selection import ShuffleSplit
 
-q = pd.read_csv("/Users/biatris/Desktop/Homonym/data/dulo_xmas.tsv", sep = "\t")
+q = pd.read_csv("/Users/biatris/Desktop/Homonym/data/Pila.tsv", sep = "\t")
 
 q['word_id'] = q['word_id'].apply(lambda x: x.split('_')[-1])
 print(q['word_id'].unique())
-my_hom = HomonymFeatures(q["sent"].values, q["word_id"].values, q["start"].values, q["stop"].values, target_word = "дуло")
+my_hom = HomonymFeatures(q["sent"].values, q["word_id"].values, q["start"].values, q["stop"].values, target_word = "пила")
 
-my_hom.CreateTokensCorpus(verbose = False)
+my_hom.CreateTokenCorpus(verbose = False)
 my_hom.CreatePosCorpus(verbose=False)
 my_hom.CreateLemmaCorpus(verbose = False)
 my_hom.CreateRpeCorpus(verbose = False)
+my_hom.CreateSynsetCorpus(verbose = True)
 
 #print(my_hom.fulldata_words.to_string())
 my_hom.fulldata_words.to_csv('/Users/biatris/Desktop/Homonym/data/fulldata_words_big_df.csv')
 pos_global_features = my_hom.CreatePosFeature(look = 5)
 pos_local_features = my_hom.CreatePosFeature(look = 1)
-lemma_features = my_hom.CreateLemmaFeature(look = 3)
-nearest_pos_features = my_hom.CreateNearestPosFeature(look = 5)
+nearest_pos_features = my_hom.CreateNearestPosFeature(look = 3)
 rpe_features = my_hom.CreateRpeFeature(look = 5)
 prev_markov_prob_features = my_hom.CreatePrevProbFeature(verbose = False)
 next_markov_prob_features = my_hom.CreateNextProbFeature(verbose = False)
 
 #Xtot = pd.concat((prev_markov_prob_features, next_markov_prob_features), axis = 1)
-Xtot = pd.concat((pos_global_features, pos_local_features, lemma_features, nearest_pos_features, rpe_features), axis = 1)
+#Xtot = pd.concat((pos_global_features, pos_local_features, lemma_features, nearest_pos_features, rpe_features), axis = 1)
 #print(Xtot)
 Y = my_hom.fulldata['target_pos_label'].values
 
 res = []
 rounds = 10
 
-    """for k in range(rounds):
-    X_train, X_test, y_train, y_test = train_test_split(Xtot, Y, test_size=0.2)
-    mysgd = SGDClassifier(loss="hinge", penalty="elasticnet", tol=1e-5, n_jobs=-1, max_iter=10000, eta0=0.0000001, alpha=1e-3)
-    mysgd.fit(X_train, y_train)
-    res.append(mysgd.score( X_test, y_test ) )"""
-
-X_aug = np.c_[Xtot, q[["sent"]].values]
-X_train, X_test, y_train, y_test = train_test_split(X_aug, Y, test_size=0.2)
+"""for k in range(rounds):
+X_train, X_test, y_train, y_test = train_test_split(Xtot, Y, test_size=0.2)
 mysgd = SGDClassifier(loss="hinge", penalty="elasticnet", tol=1e-5, n_jobs=-1, max_iter=10000, eta0=0.0000001, alpha=1e-3)
-X_test_sent = X_test[:,-1:][:,0]
-X_test = X_test[:,:-1]
-X_train = X_train[:,:-1]
 mysgd.fit(X_train, y_train)
-res.append(mysgd.score( X_test, y_test ) )
-preds = mysgd.predict(X_test)
-res_df = pd.DataFrame()
-res_df["sent"] = X_test_sent
-res_df["ground_truth"] = y_test
-res_df["preds"] = preds
-res_df["success"] = (res_df["preds"] == res_df["ground_truth"])
+res.append(mysgd.score( X_test, y_test ) )"""
+
+
+#X_train, X_test, y_train, y_test = train_test_split(X_aug, Y, test_size=0.2)
+rs = ShuffleSplit(n_splits=6, test_size=.2, random_state=0)
+for train_index, test_index in rs.split(pos_global_features.values):
+    my_hom.FitLemmaFeature(train_indices = train_index)
+    my_hom.FitWordNetFeature(num_clues = 30, train_indices = train_index)
+    lemma_features = my_hom.TransformLemmaFeature(look = 5)
+    word_net_features_noun = my_hom.TransformWordNetFeature(pos_class = 'noun')
+    word_net_features_verb = my_hom.TransformWordNetFeature(pos_class = 'verb')
+
+    #print(pos_global_features.shape, pos_local_features.shape, nearest_pos_features.shape, rpe_features.shape, prev_markov_prob_features.shape, next_markov_prob_features.shape, word_net_features_noun.shape, word_net_features_verb.shape)
+    Xtot = pd.concat((pos_global_features, pos_local_features, nearest_pos_features, rpe_features, lemma_features, prev_markov_prob_features, next_markov_prob_features, word_net_features_noun, word_net_features_verb), axis = 1)
+
+    X_aug = np.c_[Xtot, q[["sent"]].values]
+    print(q.max())
+    X_train, X_test, y_train, y_test = X_aug[train_index,:], X_aug[test_index,:], Y[train_index], Y[test_index]
+
+    mysgd = SGDClassifier(loss="hinge", penalty="elasticnet", tol=1e-5, n_jobs=-1, max_iter=10000, eta0=0.0000001, alpha=1e-3)
+    X_test_sent = X_test[:,-1:][:,0]
+    X_test = X_test[:,:-1]
+    X_train = X_train[:,:-1]
+    print(X_train.shape[0] / X_train.shape[1], X_train.shape[0], X_train.shape[1])
+
+    mysgd.fit(X_train, y_train)
+    res.append(mysgd.score( X_test, y_test ) )
+    preds = mysgd.predict(X_test)
+    res_df = pd.DataFrame()
+    res_df["sent"] = X_test_sent
+    res_df["ground_truth"] = y_test
+    res_df["preds"] = preds
+    res_df["success"] = (res_df["preds"] == res_df["ground_truth"])
 Q = confusion_matrix(y_test, preds)
 print(Q)
-score, var = round(np.mean(res),5), round(np.var(res), 5 )
+score, var = np.mean(res), np.var(res)
 baseline = my_hom.fulldata['target_pos_label'].value_counts().max() / len(my_hom.fulldata)
-print(score, var, baseline, score/baseline)
+score = round(score, 2)
+var = round(var,7)
+baseline = round(baseline, 2)
+ratio = round(score/baseline, 2)
+print(f"score: {score}, variance: {var}, baseline: {baseline}, score over baseline: {ratio}")
 res_df.style.set_properties(subset=['text'], **{'width': '3000px'})
 pd.set_option('display.max_colwidth', -1)
 res_df.to_html("myresult.html")
-target_word = "дуло"
+target_word = "пила"
 with fileinput.FileInput("/Users/biatris/Desktop/Homonym/results/myresult.html", inplace=True) as f:
     for line in f:
         print(line.replace(target_word, f" <span style='color: red'> {target_word} </span>"), end='')
